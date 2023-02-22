0f043db4f6188c65ede7b0353f22c069f2b397ed
Yang ZHANG <y.zhang03@outlook.com>
2023-02-22 02:52:19 +0100

    model_1

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -1,44 +1,61 @@
-from sklearn.base import BaseEstimator
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential 
+from keras.layers import ConvLSTM2D, BatchNormalization, LeakyReLU
+from keras.layers.convolutional import Conv3D
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def compute_rolling_std(X_df, feature, time_window, center=False):
-    """
-    For a given dataframe, compute the standard deviation over
-    a defined period of time (time_window) of a defined feature
+re_height=175
+re_width=175
 
-    Parameters
-    ----------
-    X : dataframe
-    feature : str
-        feature in the dataframe we wish to compute the rolling std from
-    time_window : str
-        string that defines the length of the time window passed to `rolling`
-    center : bool
-        boolean to indicate if the point of the dataframe considered is
-        center or end of the window
-    """
-    name = "_".join([feature, time_window, "std"])
-    X_df[name] = X_df[feature].rolling(time_window, center=center).std()
-    X_df[name] = X_df[name].ffill().bfill()
-    X_df[name] = X_df[name].astype(X_df[feature].dtype)
-    return X_df
+def create_model():
+    model = Sequential()
+    model.add(ConvLSTM2D(filters=64, kernel_size=(7, 7),
+                    input_shape=(18,re_width,re_height,1),
+                    padding='same',activation=LeakyReLU(alpha=0.01), 
+                    return_sequences=True))
+    model.add(BatchNormalization())
+    model.add(ConvLSTM2D(filters=64, kernel_size=(5, 5),
+                    padding='same',activation=LeakyReLU(alpha=0.01), 
+                    return_sequences=True))
+    model.add(BatchNormalization())
+    model.add(ConvLSTM2D(filters=64, kernel_size=(3, 3),
+                    padding='same',activation=LeakyReLU(alpha=0.01), 
+                    return_sequences=True))
+    model.add(BatchNormalization())
+    model.add(ConvLSTM2D(filters=64, kernel_size=(1, 1),
+                    padding='same',activation=LeakyReLU(alpha=0.01), 
+                    return_sequences=True))
+    model.add(BatchNormalization())
+    model.add(Conv3D(filters=1, kernel_size=(3,3,3),
+                activation='sigmoid',
+                padding='same', data_format='channels_last'))
+    return model
 
+class classifier():
+  def __init__(self):
+    model = create_model()
+    model.compile(loss='binary_crossentropy', optimizer='adadelta')
+    self.epochs = 20
+    self.batch_size = 1
+    self.model = model
+  
+  def fit(self, X, y):
+    self.model.fit(
+        X,
+        y,
+        batch_size = self.batch_size,
+        epochs = self.epochs,
+        validation_data = None,
+        verbose = 1,
+    )
 
-class FeatureExtractor(BaseEstimator):
-    def fit(self, X, y):
-        return self
-
-    def transform(self, X):
-        return compute_rolling_std(X, "Beta", "2h")
-
+  def predict(self, X):
+    return self.model.predict(X)
 
 def get_estimator():
-    feature_extractor = FeatureExtractor()
-
-    classifier = LogisticRegression(max_iter=1000)
-
-    pipe = make_pipeline(feature_extractor, StandardScaler(), classifier)
-    return pipe
+  model = classifier()
+  return model
